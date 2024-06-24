7e5f3345ede10c45bf3283d4215d3d55efa4956a
BALOCH-14 <138492420+Baloch-14@users.noreply.github.com>
2024-06-24 13:41:30 +0500

    Update Baloch.py

--- Baloch.py
+++ Baloch.py
@@ -5,6 +5,6 @@ print('\033[92;1m Join Whatsapp Group')
 os.system('xdg-open https://chat.whatsapp.com/FhzbKR6VpU937CcL1FcDTq')
 fbd=platform.architecture()[0]
 if fbd=="32bit":
-    __import__("B1")
+    __import__("B2")
 elif fbd=="64bit":
-    __import__("B1")
+    __import__("B2")
