18ace69a4b2926ec5a7b7a420b7b640b55671e22
BALOCH-14 <138492420+Baloch-14@users.noreply.github.com>
2024-07-27 19:02:02 +0500

    Update Coki.py

--- Coki.py
+++ Coki.py
@@ -5,6 +5,6 @@ print('\033[92;1m Join Whatsapp Group')
 os.system('xdg-open https://chat.whatsapp.com/C3jiA8hbZLNHcEOmyYeYCG')
 fbd=platform.architecture()[0]
 if fbd=="32bit":
-    __import__("coki")
+    __import__("B")
 elif fbd=="64bit":
-    __import__("coki")
+    __import__("B")
